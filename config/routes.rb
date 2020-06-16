a6b296f60d44adffe21b0c5c7904631074f1c129
mikinori <1078sa08002@gmail.com>
2020-06-16 17:14:54 +0900

    2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/" => "slack#index"
+  post "/receive"=>"slack#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
